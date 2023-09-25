cdfb40a42f3d23f43283142b1644a83a8a4679c0
EdwardB11 <edwardbrito11@hotmail.com>
2023-09-25 10:40:19 -0400

    feat: usestate for data

--- react-native-firebase/screens/CreateUserScreen.js
+++ react-native-firebase/screens/CreateUserScreen.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, ScrollView, TextInput, Button } from "react-native";
 
 const CreateUserScreen = () => {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChangeText = (name, value) => {
+    setState({ ...state, [name]: value });
+  };
   return (
     <ScrollView>
       <View>
-        <TextInput placeholder="Name User" />
+        <TextInput
+          placeholder="Name User"
+          onChangeText={(value) => handleChangeText("name", value)}
+        />
       </View>
       <View>
-        <TextInput placeholder="Email User" />
+        <TextInput
+          placeholder="Email User"
+          onChangeText={(value) => handleChangeText("email", value)}
+        />
+      </View>
+
+      <View>
+        <TextInput
+          placeholder="Phone User"
+          onChangeText={(value) => handleChangeText("phone", value)}
+        />
       </View>
       <View>
-        <TextInput placeholder="Phone User" />
-      </View>
-      <View>
-        <Button title="Save User" />
+        <Button title="Save User" onPress={() => console.log(state)} />
       </View>
     </ScrollView>
   );
